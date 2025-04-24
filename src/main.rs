c48ef7967b3abb1c505df96be853a012a17e6a97
aw-andre <aw.andregerard@gmail.com>
2025-04-23 20:21:08 -0400

    feat: create HTML templates

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,48 @@
-use axum::{Router, routing::get};
+use anyhow::Context;
+use askama::Template;
+use axum::{
+    Router,
+    http::StatusCode,
+    response::{Html, IntoResponse, Response},
+    routing::get,
+};
 use tracing::info;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
+async fn hello() -> impl IntoResponse {
+    let template = HelloTemplate {};
+    HtmlTemplate(template)
+}
+
+#[derive(Template)]
+#[template(path = "hello.html")]
+struct HelloTemplate;
+
+/// A wrapper type that we'll use to encapsulate HTML parsed by askama into valid HTML for axum to serve.
+struct HtmlTemplate<T>(T);
+
+/// Allows us to convert Askama HTML templates into valid HTML for axum to serve in the response.
+impl<T> IntoResponse for HtmlTemplate<T>
+where
+    T: Template,
+{
+    fn into_response(self) -> Response {
+        // Attempt to render the template with askama
+        match self.0.render() {
+            // If we're able to successfully parse and aggregate the template, serve it
+            Ok(html) => Html(html).into_response(),
+            // If we're not, return an error or some bit of fallback HTML
+            Err(err) => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                format!("Failed to render template. Error: {}", err),
+            )
+                .into_response(),
+        }
+    }
+}
+
 #[tokio::main]
-async fn main() {
+async fn main() -> anyhow::Result<()> {
     tracing_subscriber::registry()
         .with(
             tracing_subscriber::EnvFilter::try_from_default_env()
@@ -11,5 +50,17 @@ async fn main() {
         )
         .with(tracing_subscriber::fmt::layer())
         .init();
-    info!("Hello, World!");
+
+    info!("initializing router...");
+
+    let router = Router::new().route("/", get(hello));
+    let port = 8000_u16;
+    let addr = std::net::SocketAddr::from(([0, 0, 0, 0], port));
+
+    info!("router initialized, now listening on port {}", port);
+
+    let listener = tokio::net::TcpListener::bind(&addr).await.unwrap();
+    axum::serve(listener, router).await.unwrap();
+
+    Ok(())
 }
