bbdc7c2122540e4b53f6956cdb61882f00004473
aw-andre <aw.andregerard@gmail.com>
2025-04-23 20:37:06 -0400

    build: use custom flake and add pnpm

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "Rust/HTMX Dev Shell";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+  outputs = { nixpkgs, ... }:
+    let pkgs = nixpkgs.legacyPackages."x86_64-linux";
+    in {
+      devShells."x86_64-linux".default = pkgs.mkShell {
+        packages = with pkgs; [ gcc rustc clippy cargo pnpm ];
+        shellHook = ''
+          export RUST_BACKTRACE=1
+        '';
+      };
+    };
+}
