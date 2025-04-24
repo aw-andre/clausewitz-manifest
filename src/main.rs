a8cea92736c834a73cf8c07fc5d7aa74c38e1ece
aw-andre <aw.andregerard@gmail.com>
2025-04-24 01:10:41 -0400

    feat: serve tailwind-styled code

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use axum::{
     response::{Html, IntoResponse, Response},
     routing::get,
 };
+use tower_http::services::ServeDir;
 use tracing::info;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
@@ -53,9 +54,13 @@ async fn main() -> anyhow::Result<()> {
 
     info!("initializing router...");
 
-    let router = Router::new().route("/", get(hello));
+    let assets_path = std::env::current_dir().unwrap();
     let port = 8000_u16;
     let addr = std::net::SocketAddr::from(([0, 0, 0, 0], port));
+    let router = Router::new().route("/", get(hello)).nest_service(
+        "/assets",
+        ServeDir::new(format!("{}/assets", assets_path.to_str().unwrap())),
+    );
 
     info!("router initialized, now listening on port {}", port);
 
