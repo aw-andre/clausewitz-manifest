4130491ffa3bc042c453ef931dbe569f7a8f9b2d
aw-andre <aw.andregerard@gmail.com>
2025-04-27 20:44:53 -0400

    feat: create basic working form

--- src/templates.rs
+++ src/templates.rs
@@ -3,6 +3,7 @@ use axum::{
     http::StatusCode,
     response::{Html, IntoResponse, Response},
 };
+use tracing::info;
 
 /// A wrapper type that we'll use to encapsulate HTML parsed by askama into valid HTML for axum to serve.
 pub struct HtmlTemplate<T>(pub T);
@@ -36,7 +37,35 @@ pub async fn index() -> impl IntoResponse {
     HtmlTemplate(template)
 }
 
+pub struct Node {
+    pub id: String,
+    pub label: String,
+    pub value: Option<f64>,
+    /// true if this node *may* have children (for lazy loading)
+    pub has_children: bool,
+}
+
+#[derive(Template)]
+#[template(path = "tree.html")]
+pub struct ModifierTreeTemplate<'a> {
+    pub modifier_key: &'a str,
+    /// A Vec of “root paths” leading down to this modifier.
+    /// Each path is a Vec<Node> from the top parent → … → the leaf modifier.
+    pub paths: Vec<Vec<Node>>,
+}
+
+#[derive(Template)]
+#[template(path = "node.html")]
+pub struct TreeNodeTemplate {
+    pub id: String,
+    pub children: Vec<Node>,
+}
+
 pub async fn modifier_tree(game: &'static str) -> impl IntoResponse {
-    let template = IndexTemplate {};
+    info!("printing modifier_tree");
+    let template = ModifierTreeTemplate {
+        modifier_key: "",
+        paths: vec![],
+    };
     HtmlTemplate(template)
 }
